b4675978b2f03917f52bdc4ab6f963dbbd431647
hanzhong.yan <center.yan@gmail.com>
2015-02-12 15:52:00 +0800

    the init script of power start

---
+++ myScript/init.sh
@@ -0,0 +1,2 @@
+("C:\Program Files\Google\Chrome\Application\chrome.exe" >/dev/null 2>1 &) && ("D:\Program Files\Tencent\QQ\QQProtect\Bin\QQProtect.exe" &) && ("D:\software\Dict\YodaoDict.exe") && (notepad++ $SHARE_FOLDER/workLog.txt &) && ("C:\Program Files\Microsoft Office\Office12\OUTLOOK.EXE"  /recycle &) && ("D:\securecrt\SecureCRT.exe" &)
+echo 'init success!Please begin today''s work!Happy to do it !!!'
